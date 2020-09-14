cacd4f4950a40e8b3a6d50992016a2aa4c7d16d8
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-09-13 22:28:22 -0300

    Removendo dados sensiveis

---
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Hcode\PagSeguro;
+
+class Config {
+
+	const SANDBOX = true;
+
+	const SANDBOX_EMAIL = "alegneto@hotmail.com";
+	const PRODUCTION_EMAIL = "alegneto@hotmail.com";
+
+	const SANDBOX_SESSIONS = "https://ws.sandbox.pagseguro.uol.com.br/v2/sessions";
+	const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/v2/sessions";
+
+	const SANDBOX_URL_JS = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+	const PRODUCTION_URL_JS = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+
+	const MAX_INSTALLMENT_NO_INTEREST = 6;
+	const MAX_INSTALLMENT = 10;
+
+	public static function getAuthentication(): array
+	{
+		if (Config::SANDBOX === true) {
+			return [
+				"email" => Config::SANDBOX_EMAIL,
+				"token" => getenv("SANDBOX_TOKEN")
+			];
+		}  else {
+			return [
+				"email" => Config::PRODUCTION_EMAIL,
+				"token" => getenv("PRODUCTION_TOKEN")
+			];
+		}
+	}
+
+	public static function gerUrlSessions(): string
+	{
+		return (Config::SANDBOX === true) ? Config::SANDBOX_SESSIONS : Config::PRODUCTION_SESSIONS;
+	}
+
+	public static function getUrlJS()
+	{
+		return (Config::SANDBOX === true) ? Config::SANDBOX_URL_JS : Config::PRODUCTION_URL_JS;
+	}
+}
