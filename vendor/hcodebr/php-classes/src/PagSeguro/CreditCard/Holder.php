5ec76c551fedc85a9bffa5f2b9a8ef6b83cf4752
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-09-15 00:07:25 -0300

    Classe Holder

--- vendor/hcodebr/php-classes/src/PagSeguro/CreditCard/Holder.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/CreditCard/Holder.php
@@ -2,11 +2,65 @@
 
 namespace Hcode\PagSeguro\CreditCard;
 
+use DateTime;
+use DOMDocument;
+use DOMElement;
+use Exception;
+use Hcode\PagSeguro\Document;
+use Hcode\PagSeguro\Phone;
+
 class Holder
 {
 
 	private $name;
-	private $documents;
+	private $document;
 	private $birthDate;
 	private $phone;
+
+	public function __construct(
+		string $name,
+		Document $document,
+		DateTime $birthDate,
+		Phone $phone)
+	{
+		
+		if (!$name) {
+
+			throw new Exception("Informe o nome do comprador.");
+
+		}
+
+		$this->name = $name;
+		$this->document = $document;
+		$this->birthDate = $birthDate;
+		$this->phone = $phone;
+
+	}
+
+	public function getDOMElement():DOMElement
+	{
+		$dom = new DOMDocument();
+
+		$holder = $dom->createElement("holder");
+		$holder = $dom->appendChild($holder);
+		
+		$name = $dom->createElement("name", $this->name);
+		$name = $holder->appendChild($name);
+		
+		$documents = $dom->createElement("documents");
+		$documents = $holder->appendChild($documents);
+		
+		$document = $this->document->getDOMElement();
+		$document = $dom->importNode($document, true);
+		$document = $documents->appendChild($document);
+		
+		$birthDate = $dom->createElement("birthDate", $this->birthDate->format("d/m/Y"));
+		$birthDate = $holder->appendChild($birthDate);
+
+		$phone = $this->phone->getDOMElement();
+		$phone = $dom->importNode($phone, true);
+		$phone = $holder->appendChild($phone);
+		
+		return $holder;
+	}
 }
