e277c8dd6920cbc6ae99851a2864b9965b2d0093
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-09-14 23:30:12 -0300

    Classe Address

--- vendor/hcodebr/php-classes/src/PagSeguro/Adress.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Adress.php
@@ -2,6 +2,10 @@
 
 namespace Hcode\PagSeguro;
 
+use DOMDocument;
+use DOMElement;
+use Exception;
+
 class Address {
 
 	private $street;
@@ -13,4 +17,101 @@ class Address {
 	private $country;
 	private $postalCode;
 
+	public function __construct(
+		string $street,
+		string $number,
+		string $complement,
+		string $district,
+		string $city,
+		string $state,
+		string $country,
+		string $postalCode)
+	{
+
+		if (!$street) {
+
+			throw new Exception("Informe o logradouro do endereço");
+
+		}
+		
+		if (!$number) {
+
+			throw new Exception("Informe o número do endereço");
+
+		}
+
+		if (!$$district) {
+
+			throw new Exception("Informe o bairro do endereço");
+
+		}
+
+		if (!$city) {
+
+			throw new Exception("Informe a cidade do endereço");
+
+		}
+
+		if (!$state) {
+
+			throw new Exception("Informe o estado do endereço");
+
+		}
+
+		if (!$country) {
+
+			throw new Exception("Informe o país do endereço");
+
+		}
+
+		if (!$postalCode) {
+
+			throw new Exception("Informe o CEP do endereço");
+
+		}
+
+		$this->street = $street;
+		$this->number = $number;
+		$this->complement = $complement;
+		$this->district = $district;
+		$this->city = $city;
+		$this->state = $state;
+		$this->country = $country;
+		$this->postalCode = $postalCode;
+		
+	}
+
+	public function getDOMElement($node = "address"):DOMElement
+	{
+		$dom = new DOMDocument();
+
+		$address = $dom->createElement($node);
+		$address = $dom->appendChild($address);
+		
+		$street = $dom->createElement("street", $this->street);
+		$street = $address->appendChild($street);
+		
+		$number = $dom->createElement("number", $this->number);
+		$number = $address->appendChild($number);
+		
+		$complement = $dom->createElement("complement", $this->complement);
+		$complement = $address->appendChild($complement);
+		
+		$district = $dom->createElement("district", $this->district);
+		$district = $address->appendChild($district);
+		
+		$city = $dom->createElement("city", $this->city);
+		$city = $address->appendChild($city);
+		
+		$state = $dom->createElement("state", $this->state);
+		$state = $address->appendChild($state);
+		
+		$country = $dom->createElement("country", $this->country);
+		$country = $address->appendChild($country);
+		
+		$postalCode = $dom->createElement("postalCode", $this->postalCode);
+		$postalCode = $address->appendChild($postalCode);
+
+		return $address;
+	}
 }
