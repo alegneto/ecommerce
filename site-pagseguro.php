e9248851d659041cffa8fbc948bf9812a2baccea
Alexandre Gonçalves Neto <alegneto@hotmail.com>
2020-09-17 23:30:48 -0300

    Efetuando pagamento Boleto

--- site-pagseguro.php
+++ site-pagseguro.php
@@ -16,6 +16,24 @@ use Hcode\PagSeguro\Sender;
 use Hcode\PagSeguro\Shipping;
 use Hcode\PagSeguro\Transporter;
 
+$app->get('/payment/success/boleto', function() {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->getFromSession();
+
+	$order->get((int)$order->getidorder());
+
+	$page = new Page();
+
+	$page->setTpl('payment-success-boleto', [
+		'order' => $order->getValues()
+	]);
+
+});
+
 $app->get('/payment/success', function() {
 
 	User::verifyLogin(false);
@@ -37,7 +55,9 @@ $app->post('/payment/credit', function() {
 	User::verifyLogin(false);
 
 	$order = new Order();
+
 	$order->getFromSession();
+
 	$order->get((int)$order->getidorder());
 
 	$birthDate = new DateTime($_POST['birth']);
@@ -125,6 +145,106 @@ $app->post('/payment/credit', function() {
 	}
 
 	$payment->setCreditCard($creditCard);
+
+	Transporter::sendTransaction($payment);
+	
+	echo json_encode([
+		"success" => true
+	]);
+
+});
+
+$app->get('/payment', function() {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+	$order->getFromSession();
+	
+	$years = [];
+	for ($y = date('Y'); $y < date('Y')+14; $y++) {
+		array_push($years, $y);
+	}
+
+	$page = new Page();
+	$page->setTpl("payment", [
+		"order" => $order->getValues(),
+		"msgError" => Order::getError(),
+		"years" => $years,
+		"pagseguro" => [
+			"urlJS" => Config::getUrlJS(),
+			"id" => Transporter::createSession(),
+			"maxInstallmentNoInterest" => Config::MAX_INSTALLMENT_NO_INTEREST,
+			"maxInstallment" => Config::MAX_INSTALLMENT
+		]
+	]);
+});
+
+$app->post('/payment/boleto', function() {
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->getFromSession();
+
+	$order->get((int)$order->getidorder());
+
+	$birthDate = new DateTime($_POST['birth']);
+
+	$address = $order->getAddress();
+
+	$cart = $order->getCart();
+
+	$cpf = new Document(Document::CPF, $_POST['cpf']);
+
+	$phone = new Phone($_POST['ddd'], $_POST['phone']);
+
+	$shippingAddress = new Address(
+		$address->getdesaddress(),
+		$address->getdesnumber(),
+		$address->getdescomplement(),
+		$address->getdesdistrict(),
+		$address->getdescity(),
+		$address->getdesstate(),
+		$address->getdescountry(),
+		$address->getdeszipcode()
+	);
+
+	$sender = new Sender(
+		$_POST['hash'],
+		$order->getdesperson(),
+		$order->getdesemail(),
+		$phone,
+		$cpf
+	);
+
+	$shipping = new Shipping(
+		$shippingAddress,
+		Shipping::PAC,
+		(float)$cart->getvlfreight()
+	);
+
+	$payment = new Payment(
+		$order->getidorder(),
+		$sender,
+		$shipping
+	);
+
+	foreach ($cart->getProducts() as $product) {
+
+		$item = new Item(
+			(int)$product['idproduct'], 
+			$product['desproduct'],
+			(int)$product['nrqtd'],
+			(float)$product['vlprice']
+		);
+
+		$payment->addItem($item);
+
+	}
+
+	$payment->setBoleto();
 
 	Transporter::sendTransaction($payment);
 	
